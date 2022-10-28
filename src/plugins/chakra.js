580c7484c3a8a6682c1401621b1257b992f9cbf0
JosueFS <josue.18rj@gmail.com>
2022-10-28 12:05:10 -0300

    style: changed default font-family and added new breakpoints

--- src/plugins/chakra.js
+++ src/plugins/chakra.js
@@ -1,30 +1,34 @@
 import Vue from 'vue';
-import Chakra, * as ChakraUi from "@chakra-ui/vue";
+import Chakra, * as ChakraUi from '@chakra-ui/vue';
 
 const customBreakpoints = {
   sm: '320px',
   md: '768px',
   lg: '960px',
   xl: '1200px',
-  '2xl': '1536px'
-}
+  '2xl': '1536px',
+};
 
 Object.entries(ChakraUi).forEach(([name, component]) => {
-  // console.log(name, component)
-  Vue.component(name, component)
+  // console.log(name, component);
+  Vue.component(name, component);
 });
 
 //HotFix: CImage and CAvatar appear to be the only components using process.browser:
 //see more: https://github.com/chakra-ui/chakra-ui-vue/issues/434
 if (typeof process === 'undefined' && typeof window.process === 'undefined') {
   window.process = {
-    browser: typeof window !== 'undefined'
-  }
+    browser: typeof window !== 'undefined',
+  };
 }
 
 Vue.use(Chakra, {
   extendTheme: {
-    breakpoints: customBreakpoints
-  }
+    breakpoints: customBreakpoints,
+    fonts: {
+      heading: 'Nunito, sans-serif',
+      body: 'Nunito, system-ui, sans-serif',
+      mono: 'Menlo, monospace',
+    },
+  },
 });
-
