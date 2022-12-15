a2d09df186535132efb788727531a30766a3510a
JosueFS <josue.18rj@gmail.com>
2022-12-15 00:42:09 -0300

    fix: adjust home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,20 @@
 <template>
-  <!-- <TheBookSection :books="books" /> -->
-
-  <BarCodeReader :readerSize="readerSize" :readerTypes="['ean_reader']" />
+  <TheBookSection :books="books" />
 </template>
 
 <script>
 import GET_BOOKS from '@/graphql/GetBooks.gql';
 
 import TheBookSection from '@/layouts/TheBookSection.vue';
-import BarCodeReader from '@/components/BarCodeReader.vue';
 
 export default {
   name: 'Home',
   components: {
     TheBookSection,
-    BarCodeReader,
   },
   data: () => ({
     books: [],
-    readerSize: {
-      width: 640,
-      height: 480,
-    },
-    detecteds: [],
   }),
-  methods: {
-    logIt(data) {
-      console.log('detected', data);
-    },
-  },
   async created() {
     const { data } = await this.$apollo.query({
       query: GET_BOOKS,
